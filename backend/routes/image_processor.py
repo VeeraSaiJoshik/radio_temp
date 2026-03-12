e4f52679bac4366bf373b8e08b1a5484f5a9f65d
Raghav Unnam <joshikdevelop@gmail.com>
2026-03-11 21:24:02 -0500

    started interface of CV functions with API; going to finish this tommorow

--- backend/routes/image_processor.py
+++ backend/routes/image_processor.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from models import BoundingBox
 from dotenv import load_dotenv
 import services.image_processor as img_processor
+import 
 
 load_dotenv()
 
@@ -15,5 +16,20 @@ router = APIRouter()
 @router.get("/get_image_id")
 def get_image_id(input: GetImageIDInput):
     input_image = img_processor.base64_to_image(input.image_base64)
+    # crop image
+    # bring back image id
+    # write the orchestration end point
     
     return {"image_id": "12345"}
+
+@router.get("/find_image_id")
+def get_image_id(input: GetImageIDInput):
+    pass
+
+@router.get("/get_user_information")
+def get_image_id(input: GetImageIDInput):
+    pass
+
+@router.get("/crop_image")
+def get_image_id(input: GetImageIDInput):
+    pass
